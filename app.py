eeb04afc85671ee8cc697038ff18be263ab53730
Sam Tate <samuel.tate@outlook.com>
2019-06-21 00:35:28 -0400

    added parens to db.read()

--- app.py
+++ app.py
@@ -6,7 +6,7 @@ import kitsuController as kc
 app = Flask(__name__, static_url_path="")
 app.register_blueprint(rc.api)
 app.register_blueprint(kc.api)
-db.read
+db.read()
 
 if __name__ == "__main__":
     app.run(debug=True)
